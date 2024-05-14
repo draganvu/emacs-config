48d0e60376eb3cd061d045ec5e1d1f43d264bd1b
draganvu <dvujovic@protonmail.ch>
2024-05-15 00:19:27 +0100

    Updated Auctex section font size

--- modules/rc-latex.el
+++ modules/rc-latex.el
@@ -1,7 +1,10 @@
-;;; rc-latex.el --- LaTeX configuration
+;;; rc-latex.el --- LaTeX Auctex configuration
 
 (use-package tex
   :ensure auctex)
+
+;; Turn off large fonts for section headings 
+(setq font-latex-fontify-sectioning 1.0)
 
 (provide 'rc-latex)
 
